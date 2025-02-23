1f97dd70ff1998753ac45c73495851629a0fc5e5
BabyJnL <ariyapanna@outlook.com>
2025-02-24 06:51:42 +0700

    ➕ feat(main): include db config into main file, add index route, and API routes

--- back-end/services/courses/src/index.js
+++ back-end/services/courses/src/index.js
@@ -5,12 +5,19 @@ const cors = require('cors');
 
 function main()
 {
+    global.db = require('./config/db')();
     const app = express();
     
     app.use(cors());
     app.use(express.json());
 
-    app.listen(process.env.PORT, () => console.log('[SERVICE] Course Service is running on ' + process.env.PORT));
+    app.get('/', (req, res) => {
+        return res.send('Hello from Course Service');
+    });
+
+    app.use('/api', require('./routes'));
+
+    app.listen(process.env.PORT, () => console.log('[SERVICE] Course Service is running on http://localhost:' + process.env.PORT));
 }
 
 main();
