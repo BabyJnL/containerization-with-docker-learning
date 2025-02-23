f8383c16c1b4e2a3556d8b1988252ca2c088845f
BabyJnL <ariyapanna@outlook.com>
2025-02-24 06:53:43 +0700

    ➕ feat(main): setup database with sequelize

---
+++ back-end/services/courses/src/config/db.js
@@ -0,0 +1,24 @@
+module.exports = async () => {
+    const { Sequelize } = require('sequelize');
+
+    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.HOST,
+        dialect: 'mysql',
+        logging: false
+    });
+
+    // Load Model
+    require('../model/Course')(sequelize);
+
+    try
+    {
+        await sequelize.authenticate();
+        console.log('[DB] Connection to database established');
+    }
+    catch(err)
+    {
+        console.error('[DB]', err.message);
+    }
+
+    return sequelize;
+}
