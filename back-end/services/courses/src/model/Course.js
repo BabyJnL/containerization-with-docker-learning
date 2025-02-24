e430beb186d83252f20f23355e6e3248e515c24f
BabyJnL <ariyapanna@outlook.com>
2025-02-24 07:06:11 +0700

    🛠 fix(main): typo in lecturer id field name

--- back-end/services/courses/src/model/Course.js
+++ back-end/services/courses/src/model/Course.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
                 autoIncrement: true,
                 primaryKey: true
             },
-            lectureId: {
+            lecturerId: {
                 type: DataTypes.INTEGER,
                 allowNull: false
             },
